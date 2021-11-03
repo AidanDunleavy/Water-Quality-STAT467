50d630c752e5d0812069adc0e8a829f284a53d99
Nicholas Ryan Solich <nrsolich@pnw.edu>
2021-11-03 15:56:01 -0500

    remove cluster code from main

--- main/Data Preprocessing.R
+++ main/Data Preprocessing.R
@@ -62,34 +62,3 @@ split = sample.split(dataset, SplitRatio = 0.70)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
-## Test Clustering
-# Using the elbow method to find the optimal number of clusters
-set.seed(123)
-wcss = vector()
-for (i in 1:10) wcss[i] = sum(kmeans(dataset, i)$withinss)
-plot(1:10,
-     wcss,
-     type = 'b',
-     main = paste('The Elbow Method'),
-     xlab = 'Number of clusters',
-     ylab = 'WCSS')
-
-# Fitting K-Means to the dataset
-set.seed(123)
-kmeans = kmeans(x = dataset, centers = 5)
-y_kmeans = kmeans$cluster
-
-# Visualizing the clusters
-library(cluster)
-clusplot(dataset,
-         y_kmeans,
-         lines = 0,
-         shade = TRUE,
-         color = TRUE,
-         labels = 2,
-         plotchar = FALSE,
-         span = TRUE,
-         main = paste('Cluster'),
-         xlab = '',
-         ylab = '')
-
